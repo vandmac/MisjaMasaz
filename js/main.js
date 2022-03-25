6c5b9385fa53a6e9ac7f8f161975202ee18114e3
Andrzej Maćkowski <vandmac@icloud.com>
2022-03-25 21:10:34 +0100

    add classes show and hide

--- js/main.js
+++ js/main.js
@@ -11,5 +11,18 @@ hamburgerMenuCloseBtn.addEventListener('click', () =>
   mainContainer.classList.remove('hamburger-menu-activated')
 );
 
+const mainNavItemStart = document.querySelector('main-nav-item-start');
+const mainNavItemOffer = document.querySelector('main-nav-item-offer');
+
+const sectionStart = document.querySelector('section-start');
+const sectionServices = document.querySelector('section-services');
+
+mainNavItemStart.addEventListener('click', () => {
+  sectionStart.classList.remove('hide');
+  sectionStart.classList.add('show');
+
+  sectionServices.classList.add('hide');
+});
+
 // const myAudioPlayer = document.getElementById('my-audio-background');
 // myAudioPlayer.volume = 0.3;
