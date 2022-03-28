15b38ffab6951b0f3d77a268a224c87c8062a7fd
Andrzej Maćkowski <vandmac@icloud.com>
2022-03-28 18:26:42 +0200

    add id to each section modify main.js to be ready for changing section through clicking in menu items

--- js/main.js
+++ js/main.js
@@ -11,5 +11,44 @@
 //   mainContainer.classList.remove('hamburger-menu-activated')
 // );
 
+const sectionStart = document.getElementById('start');
+const sectionOffer = document.getElementById('offer');
+const sectionPrices = document.getElementById('prices');
+
+const allSections = document.querySelectorAll('.section-item');
+
+const mainNavItemStart = document.querySelector('#main-nav-item-start');
+const mainNavItemOffer = document.querySelector('#main-nav-item-offer');
+const mainNavItemPrices = document.querySelector('#main-nav-item-prices');
+
+function hideSection(elements) {
+  elements = elements.length ? elements : [elements];
+  for (let index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';
+  }
+}
+
+function showSection(elements) {
+  elements = elements.length ? elements : [elements];
+  for (let index = 0; index < elements.length; index++) {
+    elements[index].style.display = '';
+  }
+}
+
+mainNavItemStart.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionStart.style.display = '';
+});
+
+mainNavItemOffer.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionOffer.style.display = '';
+});
+
+mainNavItemPrices.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionPrices.style.display = '';
+});
+
 // const myAudioPlayer = document.getElementById('my-audio-background');
 // myAudioPlayer.volume = 0.3;
