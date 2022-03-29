02b404546449c8947d8b99ba9a440c4e2ff978d9
Andrzej Maćkowski <vandmac@icloud.com>
2022-03-29 18:34:13 +0200

    repair gallery

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
-// const hamburgerMenuBtn = document.getElementById('hamburger-menu-btn');
-// const hamburgerMenuCloseBtn = document.getElementById(
-//   'hamburger-menu-close-btn'
-// );
-// const mainContainer = document.querySelector('.main-container');
-
-// hamburgerMenuBtn.addEventListener('click', () =>
-//   mainContainer.classList.add('hamburger-menu-activated')
-// );
-// hamburgerMenuCloseBtn.addEventListener('click', () =>
-//   mainContainer.classList.remove('hamburger-menu-activated')
-// );
-
 const sectionStart = document.getElementById('start');
 const sectionOffer = document.getElementById('offer');
 const sectionPrices = document.getElementById('prices');
+const sectionGallery = document.getElementById('gallery');
+const sectionContact = document.getElementById('contact');
 
 const allSections = document.querySelectorAll('.section-item');
 
 const mainNavItemStart = document.querySelector('#main-nav-item-start');
 const mainNavItemOffer = document.querySelector('#main-nav-item-offer');
 const mainNavItemPrices = document.querySelector('#main-nav-item-prices');
+const mainNavItemGallery = document.querySelector('#main-nav-item-gallery');
+const mainNavItemContact = document.querySelector('#main-nav-item-contact');
 
 function hideSection(elements) {
   elements = elements.length ? elements : [elements];
@@ -49,6 +40,20 @@ mainNavItemPrices.addEventListener('click', function () {
   hideSection(allSections);
   sectionPrices.style.display = '';
 });
+
+mainNavItemGallery.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionGallery.style.display = '';
+});
+
+mainNavItemContact.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionContact.style.display = '';
+});
+
+// initialize section start
+hideSection(allSections);
+sectionStart.style.display = '';
 
 // const myAudioPlayer = document.getElementById('my-audio-background');
 // myAudioPlayer.volume = 0.3;
