edddf365f64431e46fe923683127f1b04f91989d
Andrzej Maćkowski <vandmac@icloud.com>
2022-04-12 14:40:22 +0200

    add offer items text , hamburger menu repaired

--- js/main.js
+++ js/main.js
@@ -12,6 +12,22 @@ const mainNavItemPrices = document.querySelector('#main-nav-item-prices');
 const mainNavItemGallery = document.querySelector('#main-nav-item-gallery');
 const mainNavItemContact = document.querySelector('#main-nav-item-contact');
 
+const mainNavItemBurgerStart = document.querySelector(
+  '#main-nav-item-burger-start'
+);
+const mainNavItemBurgerOffer = document.querySelector(
+  '#main-nav-item-burger-offer'
+);
+const mainNavItemBurgerPrices = document.querySelector(
+  '#main-nav-item-burger-prices'
+);
+const mainNavItemBurgerGallery = document.querySelector(
+  '#main-nav-item-burger-gallery'
+);
+const mainNavItemBurgerContact = document.querySelector(
+  '#main-nav-item-burger-contact'
+);
+
 function hideSection(elements) {
   elements = elements.length ? elements : [elements];
   for (let index = 0; index < elements.length; index++) {
@@ -47,6 +63,31 @@ mainNavItemGallery.addEventListener('click', function () {
 });
 
 mainNavItemContact.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionContact.style.display = '';
+});
+
+mainNavItemBurgerStart.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionStart.style.display = '';
+});
+
+mainNavItemBurgerOffer.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionOffer.style.display = '';
+});
+
+mainNavItemBurgerPrices.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionPrices.style.display = '';
+});
+
+mainNavItemBurgerGallery.addEventListener('click', function () {
+  hideSection(allSections);
+  sectionGallery.style.display = '';
+});
+
+mainNavItemBurgerContact.addEventListener('click', function () {
   hideSection(allSections);
   sectionContact.style.display = '';
 });
